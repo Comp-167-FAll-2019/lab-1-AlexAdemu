316420f5e17b02d554501b1ba421572be5160439
AlexAdemu <agademujohn@aggies.ncat.edu>
2019-08-26 18:07:58 -0400

    [Alexander Ademu-John Comp 167 Lab 1]

--- Contacts/src/contacts/Contact.java
+++ Contacts/src/contacts/Contact.java
@@ -21,8 +21,38 @@ public class Contact {
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
     }
-    
+   
     public String toString(){
-        return firstName + " " + lastName  " " + phoneNumber + " " + emailAddress;
+       return getFirstName() + " " + getLastName();
     }
-}
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    }
